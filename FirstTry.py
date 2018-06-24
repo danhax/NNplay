fbd49c547a5ab7e1f6207ccb406a055332b2210c
Dan Haxton <nad_xahnot@fastmail.com>
2018-06-23 21:38:47 -0700

    more clean

--- FirstTry.py
+++ FirstTry.py
@@ -6,30 +6,30 @@ import matplotlib.pyplot as plt
 
 xrange  = 10
 nper = 4
-ndat = xrange * nper
+nx = xrange * nper
 
 batch_size = 4
 
 xdata = np.arange(xrange*nper)/nper
 
 # print(xdata.size)
-# print(ndat)
+# print(nx)
 # print(len(xdata))
-# assert ndat == len(xdata)
+# assert nx == len(xdata)
 # exit()
 
 ydata = xdata + 5 * np.sin(xdata)
 
-xdata = np.reshape(xdata,(ndat,1))
-ydata = np.reshape(ydata,(ndat,1))
+ny = len(ydata)
 
-print(xdata.size)
+xdata = np.reshape(xdata,(nx,1))
+ydata = np.reshape(ydata,(ny,1))
 
 # plt.scatter(xdata,ydata)
 # plt.show()
 
-xx = tf.placeholder(tf.float32, shape=(ndat,1))
-yy = tf.placeholder(tf.float32, shape=(ndat,1))
+xx = tf.placeholder(tf.float32, shape=(nx,1))
+yy = tf.placeholder(tf.float32, shape=(ny,1))
 
 # with tf.variable_scope('linreg'):
 
@@ -37,22 +37,23 @@ yy = tf.placeholder(tf.float32, shape=(ndat,1))
 
 A1 = tf.get_variable('A1',(1,1),
           initializer = tf.random_normal_initializer)
-b1 = tf.get_variable('b1',(1,),
+b1 = tf.get_variable('b1',(1,1),
           initializer=tf.constant_initializer(0.0))
 
 y1 = tf.matmul(xx,A1) + b1
 
-# loss = tf.reduce_sum((yy-y1)**2/ndat)
-loss   = tf.reduce_sum(tf.abs(yy-y1)/ndat)
+# loss = tf.reduce_sum((yy-y1)**2/ny)
+loss   = tf.reduce_sum(tf.abs(yy-y1)/ny)
 
 opt_operation = tf.train.AdamOptimizer().minimize(loss)
 
 with tf.Session() as sess:
   sess.run(tf.global_variables_initializer())
 
-  for _ in range(1000):
-    indices = np.random.choice(ndat,batch_size)
-    xbatch,ybatch = xdata[indices], ydata[indices]
+  for _ in range(5000):
+    # indices = np.random.choice(nx,batch_size)
+    # xbatch,ybatch = xdata[xindices], ydata[indices]
+    #
     _, loss_val = sess.run([opt_operation, loss],
                   feed_dict={xx:xdata,yy:ydata})
 
